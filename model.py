69c6d699a23748c4a27d447b3814913cdcb92b48
Brooksaranyapong <brook.saranyapong@gmail.com>
2023-12-02 11:25:53 +0700

    update add delete

--- model.py
+++ model.py
@@ -3,3 +3,20 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id:int
     item: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id":1,
+                "item": "What we doing today?"
+            }
+        }
+
+class TodoItem(BaseModel):
+    item: str
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": "What we doing today?"
+            }
+        }
--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path
-from  model import Todo
+from model   import Todo, TodoItem
 
 todo_router = APIRouter(tags=["Todo"])
 
@@ -7,6 +7,9 @@ todo_list = []
 
 @todo_router.post("/todo")
 async def add_todo(todo:Todo):
+    for todo_data in todo_list:
+        if todo_data.id == todo.id:
+            return {"message" : "Todo with ID exist!"}
     todo_list.append(todo)
     return {"message" : "To Add Successfully"}
 
@@ -15,8 +18,34 @@ async def retrieve_todos():
     return {"todos": todo_list}
 
 @todo_router.get("/todo/{todo_id}")
+
+
+@todo_router.get("/todo/{todo_id}")
 async def get_single_todo(todo_id: int = Path(..., title="The ID of todo")):
     for todo in todo_list:
         if todo.id == todo_id:
             return {"todo" : todo}
     return {"message": "Todo with ID doesn't exist!"}
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data:TodoItem,todo_id: int):
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {"message": "Todo updated successfully"}
+    return {"message": "Todo with ID doesn't exist"}
+
+@todo_router.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int):
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {"message": "Todo deleted successfully"}
+        
+    return {"message": "Todo with ID doesn't exist"}
+
+@todo_router.delete("/todo")
+async def delete_all_todo():
+    todo_list.clear()
+    return {"message": "Todo deleted successfully"}
